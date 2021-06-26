1d0f1cfcc10c12ae9e54d0eeca88e9496c253774
Utkin Eugeniy <utkineugeniy@gmail.com>
2021-06-26 21:25:23 +0700

    Calculator

--- webpack.config.js
+++ webpack.config.js
@@ -140,7 +140,7 @@ module.exports = {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
         hot: true,
-        port: 3003,
+        port: 3004,
         writeToDisk: true,
     },
     optimization: optimization()
